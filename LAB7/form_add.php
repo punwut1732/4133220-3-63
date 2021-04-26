89d13718801ed1d43b69938af6042bf4455fcadc
punsawut1732 <620112418094@bru.ac.th>
2021-04-26 09:51:00 +0700

    update

--- LAB7/form_add.php
+++ LAB7/form_add.php
@@ -10,8 +10,8 @@
     <form action="./db_add.php" method="post">
     ID : <input type="text" name="id"><br>
     Name : <input type="text" name="name"><br>
-    Surname : <input type="text" name="surname"><br>
-    Date of Birth : <input type="text" name="dob"><br>
+    Surname : <input type="text" name="sname"><br>
+    Date of Birth : <input type="date" name="dob"><br>
     <button type="submit"> SAVE </button>
     </form>
 </body>
